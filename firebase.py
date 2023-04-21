e090d035ae0e17db7e3675cf21568603896bd53b
Jonan Mathew George <jonanmathew2001@gmail.com>
2023-04-22 00:35:17 +0530

    Refactored code and modified all endpoints

--- firebase.py
+++ firebase.py
@@ -1,32 +1,34 @@
-
+# ----------------------------- Firebase file imports -----------------------------
 import firebase_admin
-from firebase_admin import credentials,auth
+from firebase_admin import credentials, auth
 import os
 from dotenv import load_dotenv
 
+# ----------------------------- File config -----------------------------
 load_dotenv()
 
-type=os.getenv("FIREBASE_TYPE")
-project_id=os.getenv("FIREBASE_PROJECT_ID")
-private_key_id=os.getenv("FIREBASE_PRIVATE_KEY_ID")
-private_key=os.getenv("FIREBASE_PRIVATE_KEY")
-client_email=os.getenv("FIREBASE_CLIENT_EMAIL")
-client_id=os.getenv("FIREBASE_CLIENT_ID")
-auth_uri=os.getenv("FIREBASE_AUTH_URI")
-token_uri=os.getenv("FIREBASE_TOKEN_URI")
-auth_provider=os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
-client_cert=os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+type = os.getenv("FIREBASE_TYPE")
+project_id = os.getenv("FIREBASE_PROJECT_ID")
+private_key_id = os.getenv("FIREBASE_PRIVATE_KEY_ID")
+private_key = os.getenv("FIREBASE_PRIVATE_KEY")
+client_email = os.getenv("FIREBASE_CLIENT_EMAIL")
+client_id = os.getenv("FIREBASE_CLIENT_ID")
+auth_uri = os.getenv("FIREBASE_AUTH_URI")
+token_uri = os.getenv("FIREBASE_TOKEN_URI")
+auth_provider = os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL")
+client_cert = os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
 
+# ----------------------------- Firebase app initialize -----------------------------
 cred = credentials.Certificate({
-  "type": type,
-  "project_id": project_id,
-  "private_key_id": private_key_id,
-  "private_key": private_key.replace("\\n", "\n"),
-  "client_email": client_email,
-  "client_id": client_id,
-  "auth_uri": auth_uri,
-  "token_uri": token_uri,
-  "auth_provider_x509_cert_url": auth_provider,
-  "client_x509_cert_url": client_cert,
+    "type": type,
+    "project_id": project_id,
+    "private_key_id": private_key_id,
+    "private_key": private_key.replace("\\n", "\n"),
+    "client_email": client_email,
+    "client_id": client_id,
+    "auth_uri": auth_uri,
+    "token_uri": token_uri,
+    "auth_provider_x509_cert_url": auth_provider,
+    "client_x509_cert_url": client_cert,
 })
 firebase_admin.initialize_app(cred)
--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-from flask import Flask, jsonify, request
+# ----------------------------- App imports -----------------------------
 import os
-import datetime
+from datetime import datetime
+from flask import Flask, jsonify, request
 from pymongo import MongoClient
 from firebase import auth
 from functools import wraps
 from dotenv import load_dotenv
 from flask_cors import CORS
 
+# ----------------------------- App config -----------------------------
 app = Flask(__name__)
-
-CORS(app, origins=['http://localhost:3000', 'https://classio.vercel.app'], supports_credentials=True)
 
 load_dotenv()
 
+# ----------------------------- CORS -----------------------------
+client_url = os.getenv("CLIENT_URL")
+CORS(app, origins=client_url.split('|'), supports_credentials=True)
+
+# ----------------------------- DB config -----------------------------
 mongodb_uri = os.getenv("MONGODB_URI")
 client = MongoClient(mongodb_uri)
 message_db = client.classio_message_db
 message_collection = message_db.messages
-main_db= client.classio_main_db
+main_db = client.classio_main_db
 student_collection = main_db.students
 teacher_collection = main_db.teachers
+
+
+# ----------------------------- Auth verification middleware -----------------------------
+
 
 def auth_verify_token(func):
     @wraps(func)
@@ -27,149 +36,173 @@ def auth_verify_token(func):
         id_token = request.headers.get('Authorization')
         if not id_token:
             return jsonify({'error': 'Missing token'}), 401
+        if id_token.startswith("Bearer "):
+            id_token = id_token[len("Bearer "):]
         decoded_token = auth.verify_id_token(id_token)
         if not id_token:
             return jsonify({'error': 'User Verification Failed'}), 401
-        uid = decoded_token['user_id']
-        name=decoded_token['name']
-        code=name.split("|")[1]
-        user_type=name.split("|")[0]
-        request.code=code
-        request.uid=uid
-        request.user_type=user_type
+        user_id = decoded_token['user_id']
+        name = decoded_token['name']
+        user_type = name.split("|")[0]
+        user_code = name.split("|")[1]
+        request.user_code = user_code
+        request.user_id = user_id
+        request.user_type = user_type
         return func(*args, **kwargs)
-
     return middleware
+
+# ----------------------------- Other Functions -----------------------------
+
+
+def get_sender_id():
+    user_id = request.user_id
+    user_type = request.user_type
+    sender_id = ''
+    if user_type == 'student':
+        sender_id = student_collection.find_one({'uid': user_id}, {'_id': 1})
+    elif user_type == 'teacher':
+        sender_id = teacher_collection.find_one({'uid': user_id}, {'_id': 1})
+    return str(sender_id['_id'])
+
+# ----------------------------- Routes and Controllers -----------------------------
+
 
 @app.route('/api/get-users', methods=['GET'])
 @auth_verify_token
 def get_users():
-    code = request.code  
-    students = student_collection.find({'code': code},{"uid":1})
-    teachers = teacher_collection.find({'code': code},{"uid":1})
-    users=[]
+    user_code = request.user_code
+    students = student_collection.find({'code': user_code}, {"uid": 1})
+    teachers = teacher_collection.find({'code': user_code}, {"uid": 1})
+    users = []
     for student in list(students):
-        user=auth.get_user(student["uid"])
-        users.append({"userID":str(student["_id"]),"displayName":user.display_name.split("|")[2],"photoURL":user.photo_url})
+        user = auth.get_user(student["uid"])
+        users.append({"userId": str(student["_id"]), "name": user.display_name.split(
+            "|")[2], "photoURL": user.photo_url})
     for teacher in list(teachers):
-        user=auth.get_user(teacher["uid"])
-        users.append({"userID":str(teacher["_id"]),"displayName":user.display_name.split("|")[2],"photoURL":user.photo_url})
+        user = auth.get_user(teacher["uid"])
+        users.append({"userId": str(teacher["_id"]), "name": user.display_name.split(
+            "|")[2], "photoURL": user.photo_url})
     return jsonify({'users': users})
 
-    
-@app.route("/api/add-message", methods=["POST"])
+
+@app.route("/api/send-message", methods=["POST"])
 @auth_verify_token
-def store_message():
-    data = request.json
-    sender_id = request.args.get('senderId')  
+def send_message():
+    sender_id = get_sender_id()
     receiver_id = request.args.get('receiverId')
+    data = request.json
     message = data.get('message')
-    created_at = datetime.datetime.utcnow().isoformat()    
+    created_at = datetime.utcnow()
     message_doc = {
         'senderId': sender_id,
         'receiverId': receiver_id,
         'message': message,
-        'createdAt':created_at,
-        'read': False    
+        'createdAt': created_at,
+        'read': False
     }
     message_collection.insert_one(message_doc)
-    return jsonify({'addedMessage': True})
-    
+    return jsonify({'sentMessage': True})
+
+
 @app.route('/api/get-chats', methods=['GET'])
 @auth_verify_token
 def get_chats():
-    sender_id = request.args.get('senderId')
-    messagess = message_collection.find({'senderId': sender_id},{"_id":0})
-    receiver_ids=[]
+    sender_id = get_sender_id()
+    messagess = message_collection.find({'senderId': sender_id}, {"_id": 0})
+    receiver_ids = []
     for msg in messagess:
         receiver_id = msg['receiverId']
-        created_at = msg['createdAt']
-        read = msg['read']
         receiver_ids.append(receiver_id)
     final_receiver_ids = list(set(receiver_ids))
-    created_at_arr=[]
-    unseen_arr=[]
+    created_at_arr = []
+    unread_arr = []
     for ri in final_receiver_ids:
-        mssgss = message_collection.find({'senderId': sender_id,'receiverId': ri},{"_id":0})
-        messages_list=list(mssgss)
+        mssgss = message_collection.find(
+            {'senderId': sender_id, 'receiverId': ri}, {"_id": 0})
+        messages_list = list(mssgss)
         created_at_arr.append(messages_list[-1]["createdAt"])
-        ctr=0
-        for mssg in mssgss:
-            if(mssg['read']==False):
-                ctr=1
+        ctr = 0
+        for mssg in messages_list:
+            if (mssg['read'] == False):
+                ctr = 1
                 break
-        if(ctr==1):
-            unseen_arr.append(True)
+        if (ctr == 1):
+            unread_arr.append(True)
         else:
-            unseen_arr.append(False)        
-    final_arr=[]       
-    for i,ri in enumerate(final_receiver_ids):
+            unread_arr.append(False)
+    final_arr = []
+    for i, ri in enumerate(final_receiver_ids):
         final_arr.append({
-            "id":ri,
-            "unseen":unseen_arr[i],
-            "createdAt":created_at_arr[i]
+            "userId": ri,
+            "unread": unread_arr[i],
+            "createdAt": created_at_arr[i]
         })
-    print(final_arr)
-    final_arr_sorted=sorted(final_arr,key=lambda user:user["createdAt"])      
-    return jsonify({'users': final_arr_sorted})
-                    
-        
+    final_arr_sorted = sorted(final_arr, key=lambda user: user["createdAt"])
+    return jsonify({'chats': final_arr_sorted})
+
+
 @app.route('/api/get-messages', methods=['GET'])
 @auth_verify_token
 def get_messages():
-    sender_id = request.args.get('senderId')
+    sender_id = get_sender_id()
     receiver_id = request.args.get('receiverId')
-    m = message_collection.find({'senderId': sender_id,'receiverId': receiver_id},{"_id":0})
-    message_collection.update_many({'senderId': sender_id,'receiverId': receiver_id,'read':False},{"$set":{'read':True}})
+    mssgs = message_collection.find(
+        {'senderId': sender_id, 'receiverId': receiver_id}, {"_id": 0})
     message_list = []
-    for msg in m:
+    for msg in list(mssgs):
         message_list.append({
             'message': msg['message'],
-            'createdAt': msg['createdAt']
+            'createdAt': msg['createdAt'],
+            "read": msg['read']
         })
+    message_collection.update_many(
+        {'senderId': sender_id, 'receiverId': receiver_id, 'read': False}, {"$set": {'read': True}})
     return jsonify({'messages': message_list})
+
 
 @app.route('/api/get-new-messages', methods=['GET'])
 @auth_verify_token
 def get_new_messages():
-    sender_id = request.args.get('senderId')
+    sender_id = get_sender_id()
     receiver_id = request.args.get('receiverId')
-    m = message_collection.find({'senderId': sender_id,'receiverId': receiver_id,'read':False},{"_id":0})
-    message_collection.update_many({'senderId': sender_id,'receiverId': receiver_id,'read':False},{"$set":{'read':True}})
+    mssgs = message_collection.find(
+        {'senderId': sender_id, 'receiverId': receiver_id, 'read': False}, {"_id": 0})
     message_list = []
-    for msg in m:
-        print(msg)
+    for msg in list(mssgs):
         message_list.append({
             'message': msg['message'],
-            'createdAt': msg['createdAt']
+            'createdAt': msg['createdAt'],
         })
-    return jsonify({'messages': message_list})
+    message_collection.update_many(
+        {'senderId': sender_id, 'receiverId': receiver_id, 'read': False}, {"$set": {'read': True}})
+    return jsonify({'new_messages': message_list})
 
-@app.route("/api/add-chatbot-message", methods=["POST"])
+
+@app.route("/api/send-message-chatbot", methods=["POST"])
 @auth_verify_token
-def chatbot_message():
+def send_message_chatbot():
+    sender_id = get_sender_id()
     data = request.json
-    sender_id = data.get('senderId')
     message = data.get('message')
-    created_at = datetime.datetime.utcnow().isoformat()
+    created_at = datetime.utcnow()
     message_doc = {
         'senderId': sender_id,
         'receiverId': "69",
         'message': message,
-        'createdAt':created_at,
-        'read': False
-        
+        'createdAt': created_at,
+        'read': True
     }
     message_collection.insert_one(message_doc)
     reply_doc = {
         'senderId': "69",
         'receiverId': sender_id,
         'message': message,
-        'createdAt':created_at,
+        'createdAt': created_at,
         'read': False
     }
-    return jsonify({'Message': reply_doc})
+    return jsonify({'sentMessage': True, 'replyMessage': reply_doc})
 
 
+# ----------------------------- Application run -----------------------------
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
