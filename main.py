c982432599c15793e2c55d1f9b0e92cc494c9d4d
Jonan Mathew George <71652360+jonanmathew@users.noreply.github.com>
2025-02-12 16:24:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -258,4 +258,4 @@ def send_message_chatbot():
 
 # ----------------------------- Application run -----------------------------
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
